e75e73b57bcf55c238690dd08c26e7e40cf5ae66
Tomas Kara <tomas.kara.91@gmail.com>
2021-12-13 19:40:52 +0100

    day four

---
+++ 05/05_2.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+for index, i in enumerate(data):
+    j = i.split(' -> ')
+    data[index] = j
+
+
+floor = np.zeros((1000,1000), dtype=int)
+
+
+for vent in data:
+    x1, y1 = [int(i) for i in vent[0].split(',')]
+    x2, y2 = [int(i) for i in vent[1].split(',')]
+    y=[y1,y2]
+    y.sort()
+    x=[x1,x2]
+    x.sort()
+    if y1==y2 and x1==x2:
+        floor[y1,x1] += 1
+    elif y1==y2:
+        floor[y1,x[0]:x[1]+1] += 1
+    elif x1==x2:
+        floor[y[0]:y[1]+1,x1] += 1
+    elif x1>x2 and y1<y2:
+        for corx,cory in zip(range(x[0],x[1]+1),range(y[1],y[0]-1,-1)):
+            floor[cory][corx] += 1
+    elif x1>x2 and y1>y2:
+        for corx,cory in zip(range(x[0],x[1]+1),range(y[0],y[1]+1)):
+            floor[cory][corx] += 1
+    elif x1<x2 and y1<y2:
+        for corx, cory in zip(range(x[0], x[1] + 1), range(y[0], y[1] + 1)):
+            floor[cory][corx] += 1
+    else:
+        for corx,cory in zip(range(x[0],x[1]+1),range(y[1],y[0]-1,-1)):
+            floor[cory][corx] += 1
+
+
+print((floor > 1).sum())
+
